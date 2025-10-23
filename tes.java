1fb3a940799d972f9e7aea4e014bfcb50ed0bf2e
muhammadsafarudinfirmani <muhammadsafarudinfirmani@gmail.com>
2025-10-23 23:46:59 +0700

    add declare

--- tes.java
+++ tes.java
@@ -3,5 +3,6 @@ public class tes {
     public static void main(String[] args) {
         System.out.println("woi  hehe");
         System.out.println("aduh pusing");
+        System.out.println("hey  kamu");
     }
 }
